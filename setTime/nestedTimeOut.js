6e9924ab2befbbe74f8873c4870daf2eae155380
pankajrajputiet <95834257+pankajrajputiet@users.noreply.github.com>
2022-01-14 07:24:10 +0530

    Create nestedTimeOut.js

---
+++ setTime/nestedTimeOut.js
@@ -0,0 +1,12 @@
+setTimeout(()=>{
+  console.log("3");
+  setTimeout(()=>{
+    console.log("2");
+    setTimeout(()=>{
+      console.log("1");
+      setTimeout(()=>{
+        console.log("0");
+      },1000);
+    },1000);
+  },1000);
+},1000);
